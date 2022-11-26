4c01c027bddb6a5ac3420dd5114aee79f68602b3
JirakLu <jirak.lukas@gmail.com>
2022-11-26 18:40:10 +0100

    feat: debugbar

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -27,7 +27,7 @@ class LoginController extends Controller
 
         return back()->withErrors([
             'password' => 'Zadali jste špatné heslo',
-        ]);
+        ])->withInput();
     }
 
     public function logout()
