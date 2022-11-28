e3046a8a1671b193edd6f748fabb2c779afe0526
JirakLu <jirak.lukas@gmail.com>
2022-11-28 10:29:29 +0100

    fix: fix

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,21 +8,7 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        $script = Script::whereId(1)->with(
-            [
-                "scenes",
-                "scenes.characters",
-                "scenes.sceneType",
-                "scenes.shots",
-                "scenes.shots",
-                "scenes.shots.comments",
-                "scenes.shots.monologs",
-                "scenes.shots.monologs.character",
-                "scenes.shots.shotParams",
-                "scenes.shots.shotType",
-                "scenes.shots.sounds"
-            ]
-        )->get();
+        $script = Script::whereId(1)->get();
 
         return view("pages.dashboard", compact("script"));
     }
