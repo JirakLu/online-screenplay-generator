e6d2e5b619d9da63021313e742a3de25b78fe8f6
Aleš Komárek <ales-komarek@seznam.cz>
2022-12-04 14:13:53 +0100

    feat: Script Policy, Script Controller

--- app/Http/Controllers/ScriptController.php
+++ app/Http/Controllers/ScriptController.php
@@ -2,10 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Script;
+
 class ScriptController extends Controller
 {
     public function index()
     {
         return view("pages.script");
+    }
+
+    public function delete($id)
+    {
+        // delete script with id $id
+         $script = Script::find($id);
+            $script->delete();
+        return redirect()->route("dashboard");
+
+    }
+
+    public function create()
+    {
+        //TODO: create script
+    }
+
+    public function update()
+    {
+        //TODO: update script
+    }
+
+    public function download()
+    {
+        //TODO: download script
     }
 }
